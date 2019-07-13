94da8a1220ef3658d510253a47effa20fa320aab
Eletsky Andrey <deletedotaidiot@gmail.com>
2019-07-13 20:30:00 +0300

    remove folder

--- myWorkFlow_pro/cleanProject/react+threejs/myapp/src/_Board.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-class _Board extends React.Component {
-	state = {};
-	render() {
-		return <div className="Board">{/* Our code */}</div>;
-	}
-}
-
-export default _Board;
